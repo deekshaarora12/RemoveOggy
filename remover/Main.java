50b3d1757b56df9d319e62915d2316bc61ec24af
deekshaarora12 <68547341+deekshaarora12@users.noreply.github.com>
2020-08-13 15:03:52 +0530

    Update Main.java

--- remover/Main.java
+++ remover/Main.java
@@ -5,16 +5,24 @@ import java.util.stream.Collectors;
 
 public class Main {
     public static void removeOggy(ArrayList<String> names) {
+        for (int i = 0; i < names.size(); i++) {
+            String toberemoved = "";
+            if (names.get(i).equals("Oggy")){
+                names.remove(i);
+            }
+        }
         return;
     }
+
     public static boolean oggyIsRemoved(ArrayList<String> names) {
         List<String> oggys = names.stream().filter(it -> it.contains("oggy")).collect(Collectors.toList());
         return oggys.size() == 0;
     }
+
     public static void main(String[] args) {
         ArrayList<String> names = new ArrayList<String>(Arrays.asList("nobita", "bheem", "oggy", "oggy cockroach", "ninja"));
         removeOggy(names);
-        if(oggyIsRemoved(names)) {
+        if (oggyIsRemoved(names)) {
             System.out.println("Passed!");
             System.exit(0);
         } else {
@@ -23,3 +31,4 @@ public class Main {
         }
     }
 }
+
